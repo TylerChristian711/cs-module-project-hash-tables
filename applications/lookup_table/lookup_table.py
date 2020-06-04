3903f3facd42e22885aac081aaa52597c0f45e69
Tyler Christian <tylerc71197@gmail.com>
2020-06-04 15:38:24 -0400

    two files finished lookup_table.py and expensive_seq.py

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
 # Your code here
+import math
+import random
 
 
 def slowfun_too_slow(x, y):
@@ -9,6 +11,13 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+power_cache = {}
+factorial_cache = {}
+division_cache = {}
+module_cache = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
@@ -16,6 +25,32 @@ def slowfun(x, y):
     """
     # Your code here
 
+    i = (x, y)
+    v = None
+
+    if i in power_cache:
+        v = power_cache[i]
+    else:
+        power_cache[i] = math.pow(x, y)
+        v = power_cache[i]
+
+    if i in factorial_cache:
+        v = factorial_cache[i]
+    else:
+        factorial_cache[i] = math.factorial(v)
+
+    if i in division_cache:
+        v = division_cache[i]
+    else:
+        division_cache[i] = v // (x + y)
+        v = division_cache[i]
+
+    if i in module_cache:
+        v = module_cache[i]
+    else:
+        module_cache[i] = v % 982451653
+        v = module_cache[i]
+    return v
 
 
 # Do not modify below this line!
